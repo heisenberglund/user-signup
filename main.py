cf1c397249bbabe6f7fcd8a7a905b7d01972a1d6
Bill Volz <william.volz13@gmail.com>
2017-09-25 18:39:18 -0500

    adding directs

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
+import cgi
 import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+
+app =  Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/signup')
+
+@app.route('/')
+def redir():
+    return redirect('/signup')
+
+app.run()
