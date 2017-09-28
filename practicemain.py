2e2d9ba7ee7bbe13e683daefbf6fd5f9ac78d027
Bill Volz <william.volz13@gmail.com>
2017-09-28 15:34:29 -0500

    adding errors

--- practicemain.py
+++ practicemain.py
@@ -8,23 +8,57 @@ app.config['DEBUG'] = True
 form = """
 <!doctype html>
 <html>
+    <head>
+        <style>
+            .error {{
+                color: red;
+            }}
+        </style>
+    </head>
+
     <body>
+        <h1>User Signup</h1>
     <form method="post">
-        <label>User Name:
-            <input type="text" name="user-name" />
-        </label>
-            <br>
-        <label>Password:
-            <input type="password" name="password" />
-        </label>
-            <br>
-        <label> Verify Password:
-            <input type="password" name="vpass" />
-        </label>
-            <br>
-        <label>Email:
-            <input type="email" name="email" />
-        </label>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <label for "user-name">User Name:</label>
+                    </td>
+                    <td>
+                        <input type="text" name="user-name" />
+                        <span class="error">{name_err}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Password:</label>
+                    </td>
+                    <td>
+                        <input type="password" name="password" />
+                        <span class="error">{password_err}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label> Verify Password:</label>
+                    </td>
+                    <td>
+                        <input type="password" name="vpass" />
+                        <span class="error">{passv_err}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Email:</label>
+                    </td>
+                    <td>
+                        <input type="email" name="email" />
+                        <span class="error">{email_err}</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
             <input type="submit" name="submit" value="Sign Up" />
     </form>
     </body>
@@ -33,7 +67,7 @@ form = """
 
 @app.route("/")
 def display_form():
-    return form.format(user='', password = '', vpass = '', email = '')
+    return form.format(user='', name_err='', password = '', password_err='', vpass = '', passv_err='', email = '', email_err='')
 
 @app.route("/", methods=['POST'])
 def user_signup():
@@ -47,18 +81,18 @@ def user_signup():
     email_err = ''
 
     if len(user) > 20 or len(user) < 3:
-        return 'Your user name does not fit the parameters'
+        name_err = 'Your user name does not fit the parameters'
         user = ''
     
     if len(password) > 20 or len(password) < 3:
-        return 'Your password does not fit the parameters'
+        password_err =  'Your password does not fit the parameters'
         password = ''
 
     if len(passv) > 20 or len(passv) < 3:
-        return 'Your verification does not fit the parameters'
+        passv_err = 'Your verification does not fit the parameters'
         passv = ''
     if password != passv:
-        return 'Your password and verification do not match'
+        passv_err = 'Your password and verification do not match'
 
     else:
         return 'Welcome,' + user
