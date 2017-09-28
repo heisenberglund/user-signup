d113be2391db13309106746a4ff482a0e84ed268
Bill Volz <william.volz13@gmail.com>
2017-09-27 20:54:42 -0500

    testing errors

--- practicemain.py
+++ practicemain.py
@@ -9,7 +9,7 @@ form = """
 <!doctype html>
 <html>
     <body>
-    <form>
+    <form method="post">
         <label>User Name:
             <input type="text" name="user-name" />
         </label>
@@ -19,12 +19,13 @@ form = """
         </label>
             <br>
         <label> Verify Password:
-            <input type="password name="password" />
+            <input type="password" name="vpass" />
         </label>
             <br>
         <label>Email:
-            <input type="email name="email />
+            <input type="email" name="email" />
         </label>
+            <input type="submit" name="submit" value="Sign Up" />
     </form>
     </body>
 </html>
@@ -32,6 +33,35 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
+
+@app.route("/", methods=['POST'])
+def user_signup():
+    user = request.form['user-name']
+    password = request.form['password']
+    passv = request.form['vpass']
+    email = request.form['email']
+    name_err = ''
+    password_err = ''
+    passv_err = ''
+    email_err = ''
+
+    if len(user) > 15 or len(user) < 3:
+        return 'Your user name does not fit the parameters'
+        user = ''
+    
+    if len(password) > 15 or len(password) < 3:
+        return 'Your password does not fit the parameters'
+        password = ''
+
+    if len(passv) > 15 or len(passv) < 3:
+        return 'Your verification does not fit the parameters'
+        passv = ''
+    if password != passv:
+        return 'Your password and verification do not match'
+
+    else:
+        return 'Welcome,' + user
+
 
 app.run()
